028432afff68b6cccea3ad0b689729f644901385
Akram Khan <akrammiru@gmail.com>
2018-09-12 13:46:44 +0600

    code refactored to smaller part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,17 @@ import {Todo} from './todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  newTodo: Todo = new Todo();
   constructor(private todoDataService: TodoDataService) {}
-  addTodo() {
-    this.todoDataService.addTodo(this.newTodo);
-    this.newTodo = new Todo();
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
   }
-  toggleTodoComplete(todo) {
+  onToggleTodoComplete(todo) {
     this.todoDataService.toggleTodoComplete(todo);
   }
-  removeTodo(todo) {
+  onRemoveTodo(todo) {
     this.todoDataService.deleteTodoById(todo.id);
   }
   get todos() {
